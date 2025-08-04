7b144ef4985d22b101b5fa379d8dbd0e52ddb46f
Lileput <c.pawlov2012@yandex.ru>
2025-08-04 14:11:54 +0300

    Add CRUD

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -6,5 +6,7 @@ import ru.netology.nmedia.dto.Post
 interface PostRepository {
     fun get(): LiveData<List<Post>>
     fun likeById(id: Long)
+    fun removeById(id: Long)
     fun reposts(id: Long)
+    fun save(post: Post)
 }
--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -1,14 +1,40 @@
 package ru.netology.nmedia.viewModel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemory
 
-class PostViewModel: ViewModel() {
-    private val repository : PostRepository = PostRepositoryInMemory()
+private val empty = Post(
+    id = 0,
+    author = "",
+    content = "",
+    published = "",
+    likes = 0,
+    likeByMe = false,
+)
+
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemory()
     val data = repository.get()
+    val edited = MutableLiveData(empty)
     fun like(id: Long) = repository.likeById(id)
     fun reposts(id: Long) = repository.reposts(id)
+    fun removeById(id: Long) = repository.removeById(id)
+    fun save(text: String) {
+        edited.value?.let {
+            val content = text.trim()
+            if (content != it.content) {
+                repository.save(it.copy(content = content))
+            }
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
 }

6a2d1a9352b4708b6ae71d4de91812c4c3d23d5a
Lileput <c.pawlov2012@yandex.ru>
2025-08-16 14:39:21 +0300

    Add Intents

---
+++ app/src/main/java/ru/netology/nmedia/activity/IntentHandlerActivity.kt
@@ -0,0 +1,21 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.R
+
+class IntentHandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intent_handler)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

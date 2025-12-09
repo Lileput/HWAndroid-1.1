c9990ce18c1f74539f46334d7400570ff3218f8b
Lileput <c.pawlov2012@yandex.ru>
2025-12-09 21:00:38 +0300

    Add Async in project

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -5,10 +5,21 @@ import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
     fun get(): List<Post>
-    fun likeById(id: Long)
-    fun unlikeById(id: Long)
-    fun removeById(id: Long)
+    fun likeById(id: Long, callback: RepositoryCallback<Unit>)
+    fun unlikeById(id: Long, callback: RepositoryCallback<Unit>)
+    fun removeById(id: Long, callback: RepositoryCallback<Unit>)
     fun reposts(id: Long)
-    fun save(post: Post) : Post
+    fun save(post: Post, callback: RepositoryCallback<Post>)
     fun edit(postId : Long, content : String)
+    fun getAllAsync(callback: GetAllCallback)
+
+    interface GetAllCallback {
+        fun onSuccess(posts: List<Post>)
+        fun onError(e: Exception)
+    }
+
+    interface RepositoryCallback<T> {
+        fun onSuccess(data: T)
+        fun onError(e: Exception)
+    }
 }

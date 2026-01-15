670d01107c6bc02f7d35b5224254537e6442796e
Lileput <c.pawlov2012@yandex.ru>
2026-01-15 16:23:20 +0300

    Retrofit CRUD

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,25 +1,25 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
+
 import ru.netology.nmedia.dto.Post
 
-interface PostRepository {
-    fun get(): List<Post>
-    fun likeById(id: Long, callback: RepositoryCallback<Unit>)
-    fun unlikeById(id: Long, callback: RepositoryCallback<Unit>)
-    fun removeById(id: Long, callback: RepositoryCallback<Unit>)
-    fun reposts(id: Long)
-    fun save(post: Post, callback: RepositoryCallback<Post>)
-    fun edit(postId : Long, content : String)
-    fun getAllAsync(callback: GetAllCallback)
+    interface PostRepository {
+        fun get(): List<Post>
+        fun likeById(id: Long, callback: RepositoryCallback<Unit>)
+        fun unlikeById(id: Long, callback: RepositoryCallback<Unit>)
+        fun removeById(id: Long)
+        fun reposts(id: Long)
+        fun save(post: Post)
+        fun edit(postId : Long, content : String)
+        fun getAllAsync(callback: GetAllCallback)
 
-    interface GetAllCallback {
-        fun onSuccess(posts: List<Post>)
-        fun onError(e: Exception)
-    }
+        interface GetAllCallback {
+            fun onSuccess(posts: List<Post>)
+            fun onError(e: Throwable)
+        }
 
-    interface RepositoryCallback<T> {
-        fun onSuccess(data: T)
-        fun onError(e: Exception)
+        interface RepositoryCallback<T> {
+            fun onSuccess(data: T)
+            fun onError(e: Exception)
+        }
     }
-}
